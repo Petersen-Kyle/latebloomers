7421fe6c754744f6aa4774ec5049450e995cac7b
petey82 <35268701+petey82@users.noreply.github.com>
2018-10-13 15:04:26 -0500

    corrected typo in scene

--- CityOfAaronLB/src/CityOfAaronLB/CityOfAaronLB.java
+++ CityOfAaronLB/src/CityOfAaronLB/CityOfAaronLB.java
@@ -44,6 +44,7 @@ public class CityOfAaronLB {
         gameOne.setWheatInStroage(100);
         System.out.println(gameOne.toString());
         
+<<<<<<< HEAD
         //creating Scene object
         Scene wheatfield = new Scene();
         wheatfield.setName("Wheat Field");
@@ -55,6 +56,16 @@ public class CityOfAaronLB {
         pointOne.setRow (1);
         pointOne.setColumn(1);
         System.out.println(pointOne.toString());
+=======
+        Question introText = new Question();
+        introText.questionText("What would you like to do?");
+        introText.answer1("1 - Start a new game");
+        introText.answer2("2 - Load a saved game");
+        introText.answer3("3 - Help");
+        introText.answer4("4 - Quit");
+        introText.userAnswer(1);
+        System.out.println(introText.toString());
+>>>>>>> origin/master
         
         
     }
