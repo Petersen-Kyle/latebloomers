83e2886b9cbba4690cc13a1896ef8a7820dac056
petey82 <35268701+petey82@users.noreply.github.com>
2018-10-13 15:51:53 -0500

    Merge origin/master
    Conflicts:
    	CityOfAaronLB/src/CityOfAaronLB/CityOfAaronLB.java

--- CityOfAaronLB/src/CityOfAaronLB/CityOfAaronLB.java
+++ CityOfAaronLB/src/CityOfAaronLB/CityOfAaronLB.java
@@ -1,48 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package CityOfAaronLB;
 
-import cit260.cityOfAaron.model.*;
-import cit260.cityOfAaron.model.Author;
-import java.util.HashSet;
-import java.util.Set;
-
-
-/**
- *
- * @author lafon
- */
-public class CityOfAaronLB {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //Test new player
-        Player playerOne = new Player();
-        playerOne.setName("George Jetson");
-        String playerOneName = playerOne.getName();
-        System.out.println(playerOne.toString());
-        
-        Animals.Mouse.getItemType();
-        Animals.Mouse.getDescription();
-        Animals.Mouse.getItemName();
-        Animals.Mouse.getAdjustments();
-        System.out.println(Animals.Mouse.toString());
-        
-        Author.Michael.getFirstname();
-        Author.Michael.getLastname();
-        System.out.println(Author.Michael.toString());
-        
-        Game gameOne = new Game();
-        gameOne.setAcresOwned(1000);
-        gameOne.setCurrentPopulation(100);
-        gameOne.setWheatInStroage(100);
-        System.out.println(gameOne.toString());
         
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -123,7 +79,9 @@ public class CityOfAaronLB {
         gameLocation.setVisited(true);
         System.out.println(gameLocation.toString());
      
+        System.out.println(Weapons.spear.toString());
         
+        System.out.println(Tools.plow.toString());
     }
     
 }
