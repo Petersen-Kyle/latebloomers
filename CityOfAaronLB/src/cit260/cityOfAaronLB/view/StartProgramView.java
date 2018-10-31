87422d9b4c8a6a58462b82a5ca6fc940195dce01
petey82 <35268701+petey82@users.noreply.github.com>
2018-10-30 19:37:46 -0500

    K.Petersen StartProgramView

--- CityOfAaronLB/src/cit260/cityOfAaronLB/view/StartProgramView.java
+++ CityOfAaronLB/src/cit260/cityOfAaronLB/view/StartProgramView.java
@@ -5,13 +5,49 @@
  */
 package cit260.cityOfAaronLB.view;
 
+
+
+
 /**
  *
  * @author lafon
  */
 public class StartProgramView {
     public StartProgramView(){
+            
+    boolean endOfView = false;
     
+    do {
+       
+    
+        String[] inputs = this.getInputs();                
+        if (inputs.length < 1 || inputs[0] == "Q"){
+                      
+        }        
+        endOfView = doAction(inputs);
+        
+        } 
+        while (endOfView != true); 
+        
     }
     
-}
+     private String[] getInputs(){
+        
+        String [] inputs = new String[1];
+        inputs[0] = ("testInput");
+        return inputs;
+    }
+       
+    private boolean doAction(String[] inputs){
+        System.out.println("**** doAction() called ****");
+        System.out.println("\tinputs =" + inputs[0]);
+        return true;
+        
+    }  
+                     
+    }
+    
+    
+    
+    
+
