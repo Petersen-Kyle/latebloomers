8eb2999425f287307b5e7c6287f046cb2f566d28
Pythaules <pythaules@gmail.com>
2018-12-03 18:44:00 -0500

    Merge origin/master
    Conflicts:
    	CityOfAaronLB/src/cit260/cityOfAaronLB/view/HelpMenu.java

--- CityOfAaronLB/src/cit260/cityOfAaronLB/view/MoveLocation.java
+++ CityOfAaronLB/src/cit260/cityOfAaronLB/view/MoveLocation.java
@@ -13,32 +13,31 @@ import cit260.cityOfAaronLB.model.Map;
  *
  * @author lafon
  */
-public class MoveLocation extends View{
+public class MoveLocation extends View {
 
-    
     public MoveLocation() {
         super("Enter Row(Q to quit): ");
     }
-    
+
     @Override
-    public boolean doAction(String inputs){  
+    public boolean doAction(String inputs) {
         String row = inputs;
         String col = getInput("Enter Column(Q to quit): ");
-        if (col.toUpperCase().trim().equals("Q")){
+        if (col.toUpperCase().trim().equals("Q")) {
             System.out.println("You didnt want to move...");
             return true;
         }
-      
+
         int inRow = -1;
         int inCol = -1;
         try {
             inRow = Integer.parseInt(row);
-            inCol = Integer.parseInt(col); }
-        catch (NumberFormatException e) {
+            inCol = Integer.parseInt(col);
+        } catch (NumberFormatException e) {
             System.out.println("Invalid entry, must be an number...");
             return false;
         }
-        
+
         Game game = CityOfAaronLB.CityOfAaronLB.getGame();
         Map map = game.getMap();
         try {
@@ -51,4 +50,3 @@ public class MoveLocation extends View{
     }
 
 }
-
