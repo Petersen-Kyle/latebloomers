c54390b27271b5d68b7eddceae5b75acc0b93687
mtsmith <mtsmith@TB-IT006>
2018-12-14 10:09:37 -0500

    Merge origin/master
    Conflicts:
    	CityOfAaronLB/src/cit260/cityOfAaronLB/model/Adjustments.java

--- CityOfAaronLB/src/cit260/cityOfAaronLB/control/TithesControl.java
+++ CityOfAaronLB/src/cit260/cityOfAaronLB/control/TithesControl.java
@@ -7,7 +7,7 @@ package cit260.cityOfAaronLB.control;
 
 import cit260.cityOfAaronLB.exceptions.GameControlException;
 import cit260.cityOfAaronLB.model.Game;
-import cit260.cityOfAaronLB.view.EndOfTheYear;
+import cit260.cityOfAaronLB.view.EndOfYearView;
 
 /**
  *
@@ -19,8 +19,8 @@ public class TithesControl {
         int tithesToPay = Integer.parseInt(tithesPaid);
         
         if (game.isTithesPaid()) {
-            System.out.println("You have already paid tithes.\n");
-            EndOfTheYear end = new EndOfTheYear();
+            System.out.println("You have already paid tithes this year.\n");
+            EndOfYearView end = new EndOfYearView();
             end.display();
         }
 
@@ -32,20 +32,27 @@ public class TithesControl {
 
         if (tithesToPay < 0) {
             throw new GameControlException("You need to enter a positive number");
-        } else if (tithesToPay > 100) {
-            throw new GameControlException("Number cannot be greater than 100");
-        } else if (tithesToPay <= 8) {
-            game.setTitheBonus((int) (Math.random() * 3) + 1 * game.getAcres());
-        } else if (tithesToPay >= 12) {
-            game.setTitheBonus((int) (Math.random() * 5) + 2 * game.getAcres());
+        } else if (tithesToPay > game.getWheat()) {
+            throw new GameControlException("You dont have that much Wheat");
+        } else if (((tithesToPay / game.getIncrease()) * 100) <= 8) {
+            game.setPerAcre((int) (Math.random() * 2) + 1);
+        } else if (((tithesToPay / game.getIncrease()) * 100) >= 12) {
+            game.setPerAcre((int) (Math.random() * 3) + 2);
         } else {
-            game.setTitheBonus((int) (Math.random() * 4) + 2 * game.getAcres());
+            game.setPerAcre((int) (Math.random() * 2) + 2);
         }
         
-        game.setRatsEat((int) (Math.random() * 4) + 2);
+        int chanceOfRats = ((int) (Math.random() * 100) + 1);
+        if (chanceOfRats < 30 && ((tithesToPay / game.getIncrease()) * 100) < 8 ) {
+            game.setRatsEat(game.getWheat() * (((int)(Math.random() * 4) + 6) / 100));
+        } else if (chanceOfRats < 30 && ((tithesToPay / game.getIncrease()) * 100) > 12){
+            game.setRatsEat(game.getWheat() * (((int)(Math.random() * 2) + 3) / 100));
+        } else {game.setRatsEat(game.getWheat() * (((int)(Math.random() * 4) + 3) / 100));        
+        }
 
-        game.setTithe(tithesToPay/game.getWheat() * 100);
-        game.setWheat(game.getWheat() - game.getTithe() + game.getTitheBonus() - game.getRatsEat());
+        game.setTithe(tithesToPay);
+        game.setWheat(game.getWheat() - game.getTithe());
+        game.setIncrease(1);
         return game.getWheat();
     }
 }
